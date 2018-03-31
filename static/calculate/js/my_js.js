ebe3b5fc0fa6482c5a923af9219d179970bfe2bd
shahabkamali <shahabkamalicom@gmail.com>
2018-03-31 14:02:56 +0430

    collect static

--- static/calculate/js/my_js.js
+++ static/calculate/js/my_js.js
@@ -1,31 +1,42 @@
+var lookup = function(value, text_cells, value_cells){
+    text_cells = text_cells.split(':');
+    var i1 = text_cells[0].substring(1);
+    var i2 = text_cells[1].substring(1);
+    //console.log(i1);
+    //console.log(i2);
+    var text_col = text_cells[0][0];
+    var value_col = value_cells[0][0];
+    var text_dict = {};
+    for(i=i1; i<= i2; i++){
+        text_dict[$('#' + text_col + i ).val()] = $('#' + value_col + i ).val();
+    }
+    //console.log(text_dict);
+    return text_dict[value];
+}
 $( document ).ready(function() {
-    $('#calc-btn').click(function(){
-        var A = parseFloat($('#inputA').val());
-        var B = parseFloat($('#inputB').val());
-        var C = parseFloat($('#inputC').val());
-        var D = parseFloat($('#inputD').val());
-        var E = parseFloat($('#inputE').val());
-        var F = parseFloat($('#inputF').val());
-        $('#input-result').val(eval($('#input-formula').val()));
-
-        var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
-        var data ={
-            A1 : A,
-            A2 : B,
-            A3 : C,
-            A4 : D,
-            A5 : E,
-            A6 : F,
-            'csrfmiddlewaretoken': csrftoken
-        };
-        $.ajax({
-          method: "POST",
-          url: "/calculation/ajax_req",
-          data: data
-        })
-          .done(function( msg ) {
-            alert( "Data Saved: " + msg );
-          });
+    $('.nav-tabs a:first').tab('show');
+    $(".nav-tabs a").click(function(){
+        $(this).tab('show');
     });
+
+    /// calculate formula
+    $('.index .sheet1 input').focusout(function(){
+        $( "input[type='text']" ).each(function( index ) {
+            eval($(this).attr('id') + " = $('#"+ $(this).attr('id')+"').val();");
+        });
+        
+        $( "input" ).each(function( index ) {
+            //console.log( index + ": " + $( this ).text() );
+            if ($(this).data('formula')){
+                var formula = $(this).data('formula');
+                // formula = formula.replace("/^[A-Z]",'B');
+                // console.log(formula);
+                $(this).val(eval(formula));
+            }
+        });
+    });
+    
+    
+
 
 });
